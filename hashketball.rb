19cc252531d755ce14cec659064cd18b14657a89
Matthew Welch <dmw9400@gmail.com>
2017-11-25 17:32:11 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -172,6 +172,23 @@ end
 
 def team_names
   team_name_array = []
-  game_hash.each do |information, team_info|
-    if game_hash[]
+  game_hash.each do |home_away, team_info|
+    team_name_array.push(game_hash[home_away][:team_name])
+  end
+  return team_name_array
 end
+
+
+def player_numbers(team)
+  player_number_array = []
+  game_hash.each do |home_away, team_info|
+    # player_number_array<< team_info[:players]
+    team_info[:players].each do |player_name, player_stats|
+      player_number_array<< player_stats[:number]
+      # player_number_array.push(game_hash[home_away][team_info][player_name][:number])
+    end
+  end
+  return player_number_array
+end
+
+    # player_number_array.push(game_hash[home_away][team_info][:players])
